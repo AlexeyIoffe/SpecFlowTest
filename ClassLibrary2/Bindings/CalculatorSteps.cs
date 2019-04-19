6ca7811c6a1dca2af8b2d4dec2ef730cc9de5a88
Alex Ioffe <alexei.ioffe@gmail.com>
2019-04-19 15:25:58 +0400

    Initial commit

---
+++ ClassLibrary2/Bindings/CalculatorSteps.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ClassLibrary2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private int first = 0;
+        private int second = 0;
+        private int result = 0;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            first = p0;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int p0)
+        {
+            second = p0;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = first + second;
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, result);
+        }
+    }
+}
